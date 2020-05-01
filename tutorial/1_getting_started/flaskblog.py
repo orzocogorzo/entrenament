11de63ec69a3cfa949a574c8f83296b884f67687
Lucas García <lucasgarciabaro@gmail.com>
2020-05-01 10:40:30 +0200

    TERCER VIDEO

--- tutorial/1_getting_started/flaskblog.py
+++ tutorial/1_getting_started/flaskblog.py
@@ -4,26 +4,14 @@ from flask import Flask
 app = Flask(__name__)
 
 
-posts = [
-    {
-        "author": "Corey Schafer",
-        "title": "Blog Post 1",
-        "content": "First post content",
-        "date_posted": "April 20, 2018"
-    },
-    {
-        "author": "Jane Doe",
-        "title": "Blog Post 2",
-    }
-]
 @app.route("/")
 @app.route("/home")
-def home ():
+def home (request):
     return "<h1>Home Page</h1>"
     
     
 @app.route("/about")
-def about ():
+def about (request):
     return "<h1>About Page</h1>"
     
     
